26b8e8a2ebf44637db9fdf9a724ca73589511f8b
Diksha Gupta <dikshagupta2012@gmail.com>
2023-07-14 15:25:17 +0530

    momdify recommendations

--- src/components/recommendations/RecommendationCard.jsx
+++ src/components/recommendations/RecommendationCard.jsx
@@ -12,25 +12,25 @@ const RecommendationCard = ({ recommendations }) => {
   const renderListItems = () => {
     return recommendations?.map((exchange) => {
       return (
-        <div style={{overflowWrap:"break-word", marginBottom: 30}}>
-        <li className={styles.listItem} key={exchange.stockId} style={{border:"1px solid black", borderRadius: "4px", padding: 4}}>
+        <div style={{overflow:"hidden" ,overflowWrap:"break-word", marginBottom: 30, position:"relative"}}>
+        <li className={styles.listItem} key={exchange.stockId} style={{float:"left", border:"1px solid black", borderRadius: "4px", padding: 4}}>
           <div className={styles.name} onClick={() => console.log("stock clicked")}>
           <Link to={`/`} className={styles.link}>{exchange.stockName}</Link>
           </div>
-          <div className={styles.index}>{exchange.rank}</div>
-          <span style={{whiteSpace:"pre-wrap",overflowWrap:"break-word"}}>
-            <p>high performance stock for 2</p>
-          </span>
-          <div className={styles.index}>{exchange.recommendationAction}</div>
-          <div className={exchange.recommendationAction === "buy" ? styles.buy : styles.sell} >{exchange.recommendationAction}</div>
+          {/* <div className={styles.index}>{exchange.rank}</div> */}
+          <div style={{whiteSpace:"pre-wrap",overflowWrap:"break-word"}}>
+            <p>{exchange?.reason}</p>
+          </div>
+          {/* <div className={styles.index}>{exchange.recommendationAction}</div> */}
         </li>
+        <div className={exchange.recommendationAction === "buy" ? styles.buy : styles.sell} style={{marginTop: 13, marginLeft: 10, float:"left"}}>{exchange.recommendationAction}</div>
         </div>
       );
     });
   };
 
   return (
-    <div className={styles.container} style={{maxHeight: 400, overflowY: "scroll", maxWidth: 1200, borderRadius:30, border:20, borderColor:"black",whiteSpace:"pre-wrap",overflowWrap:"break-word"}}>
+    <div className={styles.container} style={{maxHeight: 600, borderRadius:30, border:20, borderColor:"black",whiteSpace:"pre-wrap",overflowWrap:"break-word"}}>
       <ul className={styles.exchangeList}>{renderListItems()}</ul>
     </div>
   );      
